a556489c6cda3b333746b683f5b64ebd5762b6a7
amangithub9 <iamaman770@gmail.com>
2024-02-22 12:21:56 +0530

    echo/print

---
+++ print.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h1>This is me Aman</h1>";
+echo "I have to learn php in 10 days! <br>";
+echo "and I will definately learn php in 10 days!";
+echo " god is ", " also", "with ", "me in this ", "journey ";
+echo "and i'll " . "not let " . "him down " . "this time";
+
+print "<br>";
+print "<br>";
+print "Hi in php we can use print as well <br>";
+print "I am going " . " to be " . " millionaire " . " soon!";
+?>
