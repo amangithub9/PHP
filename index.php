cbd4ccb049ab73cfd1fa2bb98a614de0ce45e46c
amangithub9 <iamaman770@gmail.com>
2024-02-22 11:18:19 +0530

    Start

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+echo "Hello world \n";
+echo 11+22 ;
+echo "88";
+?>
+
+<?php 
+echo "Hello world \n";
+echo 11+22 ;
+echo "88";
+?>
+
+<?php 
+echo "Hello world \n";
+echo 11+22 ;
+echo "88";
+?>
