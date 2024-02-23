696dee0187d8f16716913eb47cb5e414f9fb35cc
amangithub9 <iamaman770@gmail.com>
2024-02-23 16:25:13 +0530

    conditonal pending

--- 05conditionals.php
+++ 05conditionals.php
@@ -25,4 +25,66 @@ elseif($age > 15){
 else{
     "only water";
 }
+
+echo "<br>";
+
+$a = 2;
+
+if($a > 4){
+    echo "a is greater number";
+}
+else{
+    echo "a is smaller";
+}
+
+echo "<br>";
+if(5 > 4){
+    echo "5 is greater";
+}
+
+echo "<br>"; 
+
+$num1 = 56;
+$num2 = 56;
+
+if($num1 == $num2){
+    echo $num1 . " is equals to ". $num2;
+}
+else{
+    echo "not equal";
+}
+
+echo "<br>";
+
+$number = array(1,2,3,4,5);
+$sum =0;
+for($i = 0; $i<count($number);$i++){
+    $sum += $i;
+}
+echo $sum;
+
+echo "<br>";
+
+$n1 = 100;
+$n2 = 200;
+$n3 = 500;
+
+if($n1 < $n2 && $n2 < $n3){
+    echo "both conditions are true";
+}
+
+echo "<br>";
+
+$a = 19;
+
+if($a < 20) $b = "Hello";
+echo $b;
+
+echo "<br>";
+
+$a =13;
+$b = $a > 15 ? "hello" : "Good Bye";
+echo $b;
+
+echo "<br>";
 ?>
