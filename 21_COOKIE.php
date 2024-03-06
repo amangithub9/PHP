1a0352afa6b2adfd12fb6d8602dd0060fc3d36fd
amangithub9 <iamaman770@gmail.com>
2024-03-06 14:48:33 +0530

    Cookie pending

--- 21_COOKIE.php
+++ 21_COOKIE.php
@@ -1,5 +1,13 @@
 <?php
-
+$cookie_name = "user";
+$cookie_value = "Aman Gupta";
+setcookie($cookie_name, $cookie_value, time() + (86400), "/");
+/*
+    // Create Cookie
+    setcookie(name, value, expire, path, domain, secure, httponly)
+    // Get Cookie
+    $_COOKIE['name']
+*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,13 +18,22 @@
 </head>
 <body>
 <?php
-/*
-    // Create Cookie
-    setcookie(name, value, expire, path, domain, secure, httponly)
-    // View Cookie
-    $_COOKIE['name']
-*/
+
+    if(!isset($_COOKIE[$cookie_name])){
+        echo "Cookie is not set yet";
+    }
+    else{
+        echo $_COOKIE[$cookie_name];
+    }
+
+    echo "<br>";
+    
+    if(count($_COOKIE) > 0) {
+    echo "Cookies are enabled.";
+    } else {
+    echo "Cookies are disabled.";
+    }
 ?>
 </body>
 </html>
-?>
+
---
+++ 21getCookie.php
@@ -0,0 +1,7 @@
+<?php
+echo "Cookie value: " . $_COOKIE["user"];
+
+
+
+?>
+
